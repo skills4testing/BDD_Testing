a0f32979f7c0d10626a7a8f355d74e1ba5822f6f
btcituk <mail.hasan@gmail.com>
2015-12-01 20:53:20 +0000

    This is initial commit to local repository

--- src/main/java/com/hascode/tutorial/cucumber/book/Library.java
+++ src/main/java/com/hascode/tutorial/cucumber/book/Library.java
@@ -18,7 +18,7 @@ public class Library {
 		Calendar end = Calendar.getInstance();
 		end.setTime(to);
 		end.roll(Calendar.YEAR, 1);
-
+ 
 		return store.stream().filter(book -> {
 			return from.before(book.getPublished()) && end.getTime().after(book.getPublished());
 		}).sorted(Comparator.comparing(Book::getPublished).reversed()).collect(Collectors.toList());
